da67f37854389f580114ebd791a8bd2b86f74bc4
Nicole <barrosnicole094@gmail.com>
2019-02-15 12:07:11 -0300

    add is staff

--- produtos/admin.py
+++ produtos/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 
 from .models import Produto
 
-admin.site.register(Produto)
+class UserAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        if request.user.is_superuser:
+            obj.is_staff = True
+            obj.save()
+
+admin.site.register(Produto, UserAdmin)
