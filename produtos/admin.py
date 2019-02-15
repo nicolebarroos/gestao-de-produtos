6f34a3d58f6f450910a04312d4fe00d756ad6aae
Nicole <barrosnicole094@gmail.com>
2019-02-15 15:19:41 -0300

    Configurações de deploy e add relacoes de bd

--- produtos/admin.py
+++ produtos/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Produto
+from .models import Produto, Codigo, Pessoas, Compra
 
 class UserAdmin(admin.ModelAdmin):
     def save_model(self, request, obj, form, change):
@@ -9,3 +9,6 @@ class UserAdmin(admin.ModelAdmin):
             obj.save()
 
 admin.site.register(Produto, UserAdmin)
+admin.site.register(Codigo)
+admin.site.register(Pessoas)
+admin.site.register(Compra)
--- produtos/models.py
+++ produtos/models.py
@@ -2,8 +2,36 @@ from django.db import models
 
 #Definição dos campos do produto
 
+class Codigo (models.Model):
+    num_cod = models.CharField( max_length = 50 )
+
+    def  __str__ (self):
+        return self.num_cod
+
+
 class Produto(models.Model):
     nome = models.CharField(max_length=30)
     categoria = models.CharField(max_length=30)
     preco = models.DecimalField(max_digits=5, decimal_places=2)
     marca = models.CharField(max_length=30)
+    cod = models.OneToOneField(Codigo, null=True, blank=True, on_delete=models.CASCADE)
+
+class Compra(models.Model):
+    protocolo = models.CharField(max_length=5, blank=True, null=True)
+    data_hora = models.DateTimeField()
+    desconto = models.DecimalField(max_digits=5, decimal_places=2)
+    def __str__(self):
+        return self.protocolo
+
+class Pessoas(models.Model):
+    nome = models.CharField(max_length=30)
+    Produto = models.ForeignKey(Produto, null=True, blank=False, on_delete=models.CASCADE)
+    cpf = models.CharField(max_length=11, blank=True, null=True)
+    endereço = models.CharField(max_length=120)
+    telefone = models.CharField(max_length=11, blank=True, null=True)
+    data = models.DateField('DD-MM-YYYY')
+    compra = models.ManyToManyField(Compra, blank=True)
+
+    def __str__(self):
+        return self.nome
+
