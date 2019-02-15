5dbb0a829dffac87d1d9f5069da89b83c1190ff5
Nicole <barrosnicole094@gmail.com>
2019-02-15 11:02:22 -0300

    migrate no heroku

--- produtos/views.py
+++ produtos/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import login
+
 from .models import Produto 
 from .forms import ProdutoForm
 
