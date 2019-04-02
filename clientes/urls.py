d5508c78f33e84ddce632a85d39d1894db7f6ff7
Nicole <barrosnicole094@gmail.com>
2019-04-02 15:08:02 -0300

    add crud

--- clientes/urls.py
+++ clientes/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import listar_clientes, criar_clientes
+from .views import listar_clientes, criar_clientes, update_clientes, delete_clientes
 
 urlpatterns = [
     path('listar/', listar_clientes, name="listar_clientes"),
     path('criar/', criar_clientes, name="criar_clientes"),
+    path('update/<int:id>/', update_clientes, name="update_clientes"),
+    path('delete/<int:id>/', delete_clientes, name="delete_clientes"),
 
 ]
--- clientes/views.py
+++ clientes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Cadastro
 from .forms import CadastroForm
 
@@ -18,3 +18,25 @@ def criar_clientes(request):
         form.save()
         return redirect('listar_clientes')
     return render(request, 'cliente_form.html', {'form': form})
+
+def update_clientes(request, id):
+    cadastro = get_object_or_404(Cadastro, pk=id)
+    form = CadastroForm(request.POST or None, request.FILES or None, instance=cadastro)
+
+    if form.is_valid():
+        form.save()
+        return redirect('listar_clientes')
+
+    return render(request, 'cliente_form.html', {'form': form})
+
+def delete_clientes(request, id):
+    cadastro = get_object_or_404(Cadastro, pk=id)
+
+    if request.method == 'POST':
+        cadastro.delete()
+        return redirect('listar_clientes')
+
+    return render(request, 'confirmar_deleção.html', {'cadastro': cadastro})
+
+
+
