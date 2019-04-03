77d251e3dd5f05041e58b3d933c761a521b3836c
Nicole <barrosnicole094@gmail.com>
2019-04-03 15:04:27 -0300

    add permissões de exemplo

--- venda/actions.py
+++ venda/actions.py
@@ -1,5 +1,11 @@
+from django.http import HttpResponseNotFound
+
 def nfe_emitida(modeladmin, request, queryset):
-    queryset.update(nfe_emitida='True')
+    if request.user.has_perm('venda.setar_nfe'):
+        queryset.update(nfe_emitida='True')
+    else:
+        return HttpResponseNotFound('<h6>Sem Permissão</h6>')
+
 
 nfe_emitida.short_description = "NF-e emitida"
 
--- venda/models.py
+++ venda/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.db.models import Sum, F, FloatField, Max
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.contrib.auth.models import Permission
 from clientes.models import Cadastro
 from produtos.models import Produto
 
@@ -13,6 +14,12 @@ class Venda(models.Model):
     impostos = models.DecimalField(max_digits=5, decimal_places=2, default=0)
     clientes = models.ForeignKey(Cadastro, null=True, blank=True, on_delete=models.PROTECT)
     nfe_emitida = models.BooleanField(default=False)
+
+    class Meta:
+        permissions = (
+            ('setar_nfe', 'Usuário pode alterar parâmetro NF-e'),
+            ('permissão 2', 'Descrição da permissão 2'),
+        )
 
     #Calculando o total do pedido com funções agregadas - Sum
     
